4017c3bd8423de95cf03dfcd1cd141e02c7b6270
Mikhail Solovev <mike@solovjov.net>
2020-08-24 02:42:29 +0300

    Pagination for Order History better be in array

--- src/Api/Account.php
+++ src/Api/Account.php
@@ -106,24 +106,18 @@ class Account extends Api
 
     /**
      * @param string|null $marketSymbol
-     * @param int|null $pageSize
-     * @param string|null $startDate
-     * @param string|null $endDate
-     * @param string|null $nextPageToken
-     * @param string|null $previousPageToken
+     * @param array|null $pagination
      * @return array
      * @throws GuzzleException
      * @throws TransformResponseException
      */
-    public function getOrderHistory(?string $marketSymbol = null, ?int $pageSize = null, ?string $startDate = null, ?string $endDate = null, ?string $nextPageToken = null, ?string $previousPageToken = null): array
+    public function getOrderHistory(?string $marketSymbol = null, ?array $pagination = null): array
     {
         $options = [];
         if (!is_null($marketSymbol)) $options['query'] = ['marketSymbol' => $marketSymbol];
-        if (!is_null($pageSize)) $options['query'] = ['pageSize' => $pageSize];
-        if (!is_null($startDate)) $options['query'] = ['startDate' => $startDate];
-        if (!is_null($endDate)) $options['query'] = ['endDate' => $endDate];
-        if (!is_null($nextPageToken)) $options['query'] = ['nextPageToken' => $nextPageToken];
-        if (!is_null($previousPageToken)) $options['query'] = ['previousPageToken' => $previousPageToken];
+        if (is_array($pagination))
+            foreach ($pagination as $key => $value)
+                $options['query'] = [$key => $value];
 
         return $this->rest('GET', '/orders/closed', $options);
     }
